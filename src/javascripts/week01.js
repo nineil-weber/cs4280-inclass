d617053aa241f04a23fef358187b58ee54fa7f78
nineil-weber <nmurrugarrallerena@weber.edu>
2022-08-30 09:56:01 -0600

    Week1 lecture

--- src/javascripts/week01.js
+++ src/javascripts/week01.js
@@ -7,7 +7,65 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 let std_name = "Nils Murrugarra-Llerena"
 document.querySelector('#std_name').innerHTML = `<strong>${std_name}</strong>`
 
-// // Variables and printing
+// In class  - lecture
+let x = 3
+console.log(x)
+
+str1 = `this 
+is a 
+multi-line string`
+
+console.log(str1)
+
+let fn ='Nils'
+let ln = 'Murrugarra'
+
+console.log(`My name is ${fn} ${ln}`)
+
+// Arrays
+let numbers = [1, 2, 3, 4]
+console.log( numbers.length )
+console.log('[Init array]')
+console.log( numbers )
+
+console.log('[Updated array]')
+console.log( numbers.map((x) => x*2) )
+
+numbers.push(5)
+numbers.unshift(0)
+console.log(numbers)
+
+for(let i=0; i<numbers.length; i++)
+{
+    console.log('Number: '+numbers[i])
+}
+
+let student =
+    {
+        w_number: 123,
+        name: 'student_1',
+        age: 20,
+        phone_number: 1234,
+    }
+
+console.log('Print Struct')
+for(let f in student){
+    console.log(`${f}: ${student[f]}`)
+}
+
+//Functions
+function getMax(x, y)
+{
+    return Math.max(x, y)
+}
+output = getMax(3, 7)
+console.log(output)
+
+const gm = (x,y) => Math.max(x, y) // Similar to Lambda Function (python)
+output_2 = gm(3, 9)
+console.log(output_2)
+
+// // [Backup] Variables and printing
 // let x = 10
 // const PI = 3.14
 //
@@ -93,48 +151,48 @@ document.querySelector('#std_name').innerHTML = `<strong>${std_name}</strong>`
 //
 // console.log(name)
 
-//////////////////////// Start with D3
-import * as d3 from "d3";
-let da = [
-  [18, 'One'],
-  [19, 'Two'],
-  [16, 'Three'],
-  [14, 'Four'],
-  [17, 'Five'],
-  [19, 'Six'],
-]
-
-d3.select('main')
-  .append('svg')
-  .attr('width',500)
-  .attr('height', 600)
-  .style('background', '#eee') // change background color
-
-//Draw rectangles
-d3.select('svg').selectAll('rect')
-   .data(da)
-   .enter()
-   .append('rect') // draw rectangle
-   .attr('x', 0) // x coordinates
-   .attr('y', function(d, i){
-     return i * 30
-   }) // y coordinates
-   .attr('height', 15)
-   .attr('width', (d)=>{
-     return d[0] * 3
-   })
-
-// Add Text
- d3.select('svg').selectAll('text')
-   .data(da)
-   .enter()
-   .append('text')
-   .attr('x', function(d){
-     return d[0] * 3 + 10
-   }) // x coordinates
-   .attr('y', function(d, i){
-     return i * 30 + 16
-   }) // y coordinates
-   .text(function(d){
-     return d[1]
-   }) // add text
+// //////////////////////// Start with D3
+// import * as d3 from "d3";
+// let da = [
+//   [18, 'One'],
+//   [19, 'Two'],
+//   [16, 'Three'],
+//   [14, 'Four'],
+//   [17, 'Five'],
+//   [19, 'Six'],
+// ]
+//
+// d3.select('main')
+//   .append('svg')
+//   .attr('width',500)
+//   .attr('height', 600)
+//   .style('background', '#eee') // change background color
+//
+// //Draw rectangles
+// d3.select('svg').selectAll('rect')
+//    .data(da)
+//    .enter()
+//    .append('rect') // draw rectangle
+//    .attr('x', 0) // x coordinates
+//    .attr('y', function(d, i){
+//      return i * 30
+//    }) // y coordinates
+//    .attr('height', 15)
+//    .attr('width', (d)=>{
+//      return d[0] * 3
+//    })
+//
+// // Add Text
+//  d3.select('svg').selectAll('text')
+//    .data(da)
+//    .enter()
+//    .append('text')
+//    .attr('x', function(d){
+//      return d[0] * 3 + 10
+//    }) // x coordinates
+//    .attr('y', function(d, i){
+//      return i * 30 + 16
+//    }) // y coordinates
+//    .text(function(d){
+//      return d[1]
+//    }) // add text
