da9af73a2465f9f7f7039a964bdf63d0acc7543d
nineil-weber <109441984+nineil-weber@users.noreply.github.com>
2022-09-27 09:40:05 -0600

    Example with all operations

--- src/javascripts/week05.js
+++ src/javascripts/week05.js
@@ -2,6 +2,34 @@ import { WebGLHelper } from './webgl_helper'
 import * as dat from 'dat.gui'
 import * as THREE from 'three'
 
+// Helpers
+function getTRSTransformation(controls){
+    let e = new THREE.Euler(
+        controls.r_x * Math.PI / 180,
+        controls.r_y * Math.PI / 180,
+        controls.r_z * Math.PI / 180
+    )
+
+    let t = new THREE.Matrix4().makeTranslation(controls.t_x, controls.t_y, controls.t_z)
+    let r = new THREE.Matrix4().makeRotationFromEuler(e)
+    let s = new THREE.Matrix4().makeScale(controls.s_x, controls.s_y, controls.s_z)
+    // let s = new THREE.Matrix4().makeTranslation(controls.s_x, controls.s_y, controls.s_z)
+
+    return new THREE.Matrix4().multiplyMatrices(t, new THREE.Matrix4().multiplyMatrices(r, s))
+
+}
+
+function getQuaternionTransformation(controls){
+    let e = new THREE.Euler(
+        controls.r_x * Math.PI / 180,
+        controls.r_y * Math.PI / 180,
+        controls.r_z * Math.PI / 180
+    )
+    let q = new THREE.Quaternion()
+    q.setFromEuler(e)
+    return new THREE.Matrix4().makeRotationFromQuaternion(q)
+}
+
 // Matrix operations
 export function vector_matrix_operations()
 {
@@ -219,7 +247,6 @@ export function displayCube(){
     gui.add(controls, 'axis', {x: 0, y: 1, z: 2})
 }
 
-
 class CubeIndexed {
     constructor(){
         this.vertices = [
@@ -336,10 +363,98 @@ export function displayCubeIndexed(){
     gui.add(controls, 'axis', {x: 0, y: 1, z: 2})
 }
 
+export function displayCubeOperations(){
+    const vs_script = `#version 300 es
+    in vec3 coordinates;
+    in vec3 color;
+    out vec4 vColor;
+    uniform mat4 transformBy;
+    void main(void) {
+      gl_Position = transformBy * vec4(coordinates, 1.0);
+      vColor = vec4(color, 1.0);
+    }  
+  `
+
+    const fs_script = `#version 300 es
+    precision mediump float;
+    in vec4 vColor;
+    out vec4 fragColor;
+    void main(void) {
+      fragColor = vColor; 
+    }
+  `
+    let canvas = document.querySelector("#webgl-scene")
+    let gl = WebGLHelper.initWebGL(canvas)
+
+    let program = WebGLHelper.initShaders(gl, vs_script, fs_script)
+    gl.useProgram(program)
+
+    let cube = new Cube()
+
+    let buffers = WebGLHelper.initBuffers(gl, program, [{
+        name: 'coordinates',
+        size: 3,
+        data: cube.v_out
+    }, {
+        name: 'color',
+        size: 3,
+        data: cube.c_out
+    }])
+
+    let transformByLoc = gl.getUniformLocation(program, 'transformBy')
+
+    let controls = {
+        t_x: 0,
+        t_y: 0,
+        t_z: 0,
+
+        r_x: 0,
+        r_y: 0,
+        r_z: 0,
+
+        s_x: 0,
+        s_y: 0,
+        s_z: 0,
+    }
+
+    function animate(){
+        WebGLHelper.clear(gl, [1, 1, 1, 1])
+
+        // Cube 1: Big cube
+        gl.uniformMatrix4fv(transformByLoc, false, getTRSTransformation(controls).elements)
+        gl.drawArrays(gl.TRIANGLES, 0, cube.v_out.length / 3)
+
+        // Cube 2: Small cube
+        let s = new THREE.Matrix4().makeScale(1, .2, 1) // Individual, without using controls
+        let q = getQuaternionTransformation(controls) // Apply rotation only
+        gl.uniformMatrix4fv(transformByLoc, false, new THREE.Matrix4().multiplyMatrices(q, s).elements)
+
+        gl.drawArrays(gl.TRIANGLES, 0, cube.v_out.length / 3)
+    }
+
+    animate()
+
+    let gui = new dat.GUI()
+    document.querySelector('aside').appendChild(gui.domElement)
+
+    gui.add(controls, 't_x').min(-1).max(1).onChange(animate)
+    gui.add(controls, 't_y').min(-1).max(1).onChange(animate)
+    gui.add(controls, 't_z').min(-1).max(1).onChange(animate)
+
+    gui.add(controls, 'r_x').min(0).max(360).onChange(animate)
+    gui.add(controls, 'r_y').min(0).max(360).onChange(animate)
+    gui.add(controls, 'r_z').min(0).max(360).onChange(animate)
+
+    gui.add(controls, 's_x').min(0.1).max(2).onChange(animate)
+    gui.add(controls, 's_y').min(0.1).max(2).onChange(animate)
+    gui.add(controls, 's_z').min(0.1).max(2).onChange(animate)
+}
+
 // Main
 vector_matrix_operations()
 // displayCube()
 // displayCubeIndexed()
+// displayCubeOperations()
 
 // Notes
 // * WebGL coordinates: https://www.tutorialspoint.com/webgl/webgl_basics.htm
